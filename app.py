44bd47472a5ff82e3889e63904a048d232bc9d36
Danesink <daniel00aiman@gmail.com>
2025-05-05 06:55:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def decrypt():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
